e7c1a55239a3814667fb1f099c1231b64eca19dc
ferrande <ferrande.lais@gmail.com>
2023-08-01 20:24:59 -0300

    removed unused variables

--- src/FiveDaysForecast.js
+++ src/FiveDaysForecast.js
@@ -1,16 +1,6 @@
 import React from "react";
 import ForecastDay from "./ForecastDay";
 
-import brokenClouds from "./assets/broken-clouds.png";
-import clear from "./assets/clear.png";
-import fewClouds from "./assets/few-clouds.png";
-import mist from "./assets/mist.png";
-import rainy from "./assets/rainy.png";
-import scatteredClouds from "./assets/scattered-clouds.png";
-import showerRain from "./assets/shower-rain.png";
-import snow from "./assets/snow.png";
-import thunderstorm from "./assets/thunderstorm.png";
-
 function FiveDaysForecast() {
     return (
         <div className="row five-day-forecasts">
