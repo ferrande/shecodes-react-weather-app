dc6e5a2137ed60657055e9599f916137ad7112f2
ferrande <ferrande.lais@gmail.com>
2023-08-19 19:39:09 -0300

    refactor: changed files location

---
+++ src/components/Body/Body.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+import SearchForm from "./SearchForm/SearchForm";
+import CompleteWeatherReport from "../CompleteWeatherReport/CompleteWeatherReport";
+
+function Body() {
+    const [currentCity, setCurrentCity] = useState("Sao Paulo");
+    const [temperature, setTemperature] = useState(21);
+
+    function handleCitySearch(city, temperature) {
+        setCurrentCity(city);
+        setTemperature(11);
+    }
+    return (
+        <body className="container">
+            <SearchForm handleSearch={handleCitySearch} />
+            <CompleteWeatherReport />
+        </body>
+    );
+}
+
+export default Body;
